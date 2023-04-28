a19fe7392417b74d9d5e676da1568a2953aa24ff
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-04-28 12:54:35 +0200

    introduce `player.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+mod player;
+
+use crate::player::Player;
+use crate::player::Side;
+
 fn main() {
     let mut player_1: Player = Player::new();
     let mut player_2: Player = Player::new();
@@ -17,53 +22,4 @@ fn print_state(player1: &Player, player2: &Player) {
     println!("{:?}", player1);
     println!("{:?}", player2);
     println!();
-}
-
-pub enum Side {
-    Left,
-    Right,
-}
-
-#[derive(Debug)]
-pub struct Player {
-    left: usize,
-    right: usize,
-}
-
-impl Player {
-    pub const fn new() -> Self {
-        Player { left: 1, right: 1 }
-    }
-
-    pub fn is_hit(&mut self, side: Side, amount: usize) {
-        match side {
-            Side::Left => self.left += amount,
-            Side::Right => self.right += amount,
-        };
-        self.left = match self.left {
-            0..=4 => self.left,
-            _ => 0,
-        };
-        self.right = match self.right {
-            0..=4 => self.right,
-            _ => 0,
-        };
-    }
-
-    pub fn n_hands(&self) -> usize {
-        (self.left > 0) as usize + (self.right > 0) as usize
-    }
-
-    fn get_hand(&self, side: Side) -> usize {
-        match side {
-            Side::Left => self.left,
-            Side::Right => self.right,
-        }
-    }
-}
-
-impl Default for Player {
-    fn default() -> Self {
-        Self::new()
-    }
 }
---
+++ src/player.rs
@@ -0,0 +1,48 @@
+#[derive(Debug)]
+pub struct Player {
+    left: usize,
+    right: usize,
+}
+
+pub enum Side {
+    Left,
+    Right,
+}
+
+impl Player {
+    pub const fn new() -> Self {
+        Player { left: 1, right: 1 }
+    }
+
+    pub fn is_hit(&mut self, side: Side, amount: usize) {
+        match side {
+            Side::Left => self.left += amount,
+            Side::Right => self.right += amount,
+        };
+        self.left = match self.left {
+            0..=4 => self.left,
+            _ => 0,
+        };
+        self.right = match self.right {
+            0..=4 => self.right,
+            _ => 0,
+        };
+    }
+
+    pub fn n_hands(&self) -> usize {
+        (self.left > 0) as usize + (self.right > 0) as usize
+    }
+
+    pub fn get_hand(&self, side: Side) -> usize {
+        match side {
+            Side::Left => self.left,
+            Side::Right => self.right,
+        }
+    }
+}
+
+impl Default for Player {
+    fn default() -> Self {
+        Self::new()
+    }
+}
