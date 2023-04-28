ea80dbc3f4e181db5857c0f7b2b18c4b499a1bfc
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-04-28 12:56:43 +0200

    make `Player` struct `Copy` and `Clone`

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,16 @@ use crate::player::Side;
 fn main() {
     let mut player_1: Player = Player::new();
     let mut player_2: Player = Player::new();
-    print_state(&player_1, &player_2);
+    print_state(player_1, player_2);
 
     player_2.is_hit(Side::Left, player_1.get_hand(Side::Right));
-    print_state(&player_1, &player_2);
+    print_state(player_1, player_2);
 
     player_1.is_hit(Side::Right, player_2.get_hand(Side::Left));
-    print_state(&player_1, &player_2);
+    print_state(player_1, player_2);
 }
 
-fn print_state(player1: &Player, player2: &Player) {
+fn print_state(player1: Player, player2: Player) {
     println!("{:?}", player1);
     println!("{:?}", player2);
     println!();
--- src/player.rs
+++ src/player.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct Player {
     left: usize,
     right: usize,
